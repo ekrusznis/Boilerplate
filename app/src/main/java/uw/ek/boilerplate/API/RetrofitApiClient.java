bd8fe4b7e4b53857929905ed9eb766ae9df358cb
ekrusznis <ekrusznis@aol.com>
2019-03-15 14:27:53 -0400

    updated with httplogger

--- app/src/main/java/uw/ek/boilerplate/API/RetrofitApiClient.java
+++ app/src/main/java/uw/ek/boilerplate/API/RetrofitApiClient.java
@@ -3,13 +3,14 @@ package uw.ek.boilerplate.API;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitApiClient {
 
     private static final String BASE_URL = "https://raw.githubusercontent.com/hasancse91/Android-CardView-RecyclerView/master/Related_Data/";
-
     private static Retrofit retrofit = null;
 
     private static Gson gson = new GsonBuilder()
@@ -19,9 +20,15 @@ public class RetrofitApiClient {
     private RetrofitApiClient() {} // So that nobody can create an object with constructor
 
     public static synchronized Retrofit getClient() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+
         if (retrofit==null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
         }
